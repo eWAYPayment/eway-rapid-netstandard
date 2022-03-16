8168a34faa7ef9a646dffa955e41a0c7503ca639
alanz-cd <73160594+alanz-cd@users.noreply.github.com>
2022-03-16 11:29:25 +0800

    Add Gpay integration test (#12)
    Add Google Gpay integration test

--- test/Eway.Rapid.IntegrationTests/DirectConnectionTests.cs
+++ test/Eway.Rapid.IntegrationTests/DirectConnectionTests.cs
@@ -288,5 +288,53 @@ namespace Eway.Rapid.IntegrationTests
             Assert.Equal("System Error", response.CodeDetails.First().DisplayMessage);
             Assert.Equal("S5000", response.CodeDetails.First().ErrorCode);
         }
+
+        [Fact]
+        public async Task GooglePay_Test()
+        {
+            var directPaymentRequest = new DirectPaymentRequest
+            {
+                PaymentInstrument = new RequestPaymentInstrument
+                {
+                    PaymentType = PaymentType.GooglePay,
+                    WalletDetails = new RequestWalletDetails
+                    {
+                        Token = "{\"signature\":\"MEYCIQCeLZcBvb6O5Bv5d1c7+5u2kVvd6wV2cePr1eQX1sfBkAIhAOnx/0pJRfxErayYd9VkF+CcfI1ZQTDVkafp8UWO9/VG\",\"intermediateSigningKey\":{\"signedKey\":\"{\\\"keyValue\\\":\\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE7yzStowZVKJVZV3r1saLJvy+uS3r6uizb79tUSy8lJEBVkjWEb4/hJGz5NeTLu4I8Wi01TzKMcqW9eaI70d/QA\\\\u003d\\\\u003d\\\",\\\"keyExpiration\\\":\\\"1647987030638\\\"}\",\"signatures\":[\"MEUCIEIrSPKDJZm3nSgFBoz1bzyGd4GptFUZpu25rAXq9lZcAiEArb2GVWuVicdysjmcQaLz+f/sLf7spyHs8ynCC2EDgrk\\u003d\"]},\"protocolVersion\":\"ECv2\",\"signedMessage\":\"{\\\"encryptedMessage\\\":\\\"cGqnnPKbE8bSGnMtsXjs9h8q/kVfbXJFtTry7Fzv3AouYe0ZJRZ+PczqQre/kmZ93gv1DT0DEO2LSeMM4T7jp3pLE+tYat6WOPW/kpcBM5CrLrOF0VFPlA3ug1MbJL/eyxv1GmdkCKV4zq95UoJEK5dDSINt3Ghy8q71GAVoXdZIR9pAZxKcm0kTQ88USzWk2oplOJ0RiC2EZFVsmHk1LGHMAW5a+p/X0As/TpLsbu53m5Zon1B6rGS7NlzOPiLGMkdVrzXYidZqv5ir+laMapWUWWGldwMzsEuyFicbqyG8fKqXpNMfxJa2wK5Hr4jnLxjuvMKVb4uKrmP893FIssgi8fVWynGuqZvyMS/ZNPSXxS8M49DALgUlhZnMuxuGxPsrCEOI+0OJ01Op+8bU+BxMXVEqOfxKxweXmvVt35Q8wzY2jK/NHN4NfmF/+8Bmb8KxkcbgrNT+3ZFTPTADIer6TA0h/RM7AioXkV2O/WW7iBNMzLx+MXgXEtbrXNndtCwrP9eQF3VnPXApsAjN5Da6jmx/rE3KoRhR8BvC9SLDIuA37KN9TOWKL34lE5FjikUOYVKMXL00YRqSHSwZ\\\",\\\"ephemeralPublicKey\\\":\\\"BHLSnSxWuWaei4YizEnN36q2EISgeWHVlE4qoVHH5w2AqyzjeAYSZWNM1MRGJ8bKtXx1Bap9GJ6v70lfDKzcfeA\\\\u003d\\\",\\\"tag\\\":\\\"O/JB6DG0CXHXBIdIQRXzNlxzZqNVEsq/aXwFmc4Yi8A\\\\u003d\\\"}\"}"
+                    }
+                },
+                Payment = new Payment
+                {
+                    TotalAmount = 100,
+                    CurrencyCode = "AUD"
+                },
+                Capture = true,
+                TransactionType = TransactionTypes.Purchase
+            };
+            var response = await _rapidClient.CreateTransaction(directPaymentRequest);
+
+            Assert.NotNull(response);
+            Assert.NotNull(response.ResponseCode);
+
+            DateTime dateTime;
+            bool tokenExpired = false;
+
+            #region Expired date calculate and compare
+            dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc).AddMilliseconds(1647584174259L).ToLocalTime();
+            tokenExpired = dateTime.CompareTo(new DateTime().ToLocalTime()) < 0;
+            #endregion
+
+            //The GPay token has expired time, so when it is fresh, we can get "A2000" success code.
+            //But when it is expired, it will return "S5017" token expired.
+            if (tokenExpired)
+            {
+                Assert.Equal("S5017", response.ResponseMessage);
+            }
+            else
+            {
+                Assert.Null(response.Errors);
+                Assert.Equal("A2000", response.ResponseMessage);
+            }
+
+        }
     }
 }
